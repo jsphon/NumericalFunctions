e4f06168f925eaf2d5fca6f1858983ddf98c154f
Jon <jonathanhon@hotmail.com>
2015-03-27 19:13:21 +0000

    Indexing Functions
    Committing some indexing changes from my laptop.

--- numba_funcs/indexing.py
+++ numba_funcs/indexing.py
@@ -14,3 +14,32 @@ def take( x, idx ):
     for i in range( idx.shape[0] ):
         r[i]=x[ idx[ i ] ]
     return r
+
+@nb.autojit
+def square_take( source, idx ):
+    """ Take from source, a 2d array """
+    r = np.empty( ( idx.shape[0], idx.shape[0] ) )
+    square_take_to_out( source, idx, r )
+    return r
+
+@nb.autojit( nopython=True )
+def square_take_to_out( source, idx, out ):
+    """ Take from source, a 2d array, with results going to out """
+    for i in range( idx.shape[0] ):
+        for j in range( idx.shape[0] ):
+            out[ i, j ] = source[ idx[i], idx[j] ]
+            
+@nb.autojit( nopython=True )
+def square_and_rect_take_to_out( source, idx0, idx1, out ):
+    """ Take from source, a 2d array, with results going to out
+        On the left is the square array from idx
+        On the right is the rect array with rows from idx0, cols from idx1
+        This is useful for optimising portfolios
+    """
+    for i in range( idx0.shape[0] ):
+        for j in range( idx0.shape[0] ):
+            out[ i, j ] = source[ idx0[i], idx0[j] ]
+        k = idx0.shape[0]
+        for j in range( idx1.shape[0] ):
+            out[ i, k ] = source[ idx0[i], idx1[j ]]
+            k+=1
--- numba_funcs/timer.py
+++ numba_funcs/timer.py
@@ -18,3 +18,21 @@ class Timer:
             print( '{1} took {0:0.3f} seconds'.format( self.interval, self.title ) )
         else:
             print( 'Timer took {0:0.3f} seconds'.format( self.interval ) )
+            
+class AccumulatedTimer( object ):
+
+    def __init__(self):
+        self.timetaken=0.0
+        self.call_count=0
+
+    def __enter__(self):
+        self.start = time.time()
+        return self        
+
+    def __exit__(self,a,b,c):
+        self.end = time.time()
+        self.interval+=self.end-self.start
+        self.call_count+=1
+    
+    def __repr__(self):
+        return '%0.2f seconds : %i calls'%(self.interval,self.call_count)
