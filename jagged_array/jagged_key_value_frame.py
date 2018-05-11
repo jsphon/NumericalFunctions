5389270cfdd00f02d0a87876d61c4d5dc1d2cd1c
jon <jonathanhon@hotmail.com>
2018-05-11 22:24:13 +0100

    Added get_ohlc_frame

---
+++ jagged_array/jagged_key_value_frame.py
@@ -0,0 +1,346 @@
+import datetime
+from collections import OrderedDict
+
+import numba as nb
+import pandas as pd
+import numpy as np
+
+from jagged_array.jagged_key_value_array import JaggedKeyValueArray, INT_TYPES
+from jagged_array.jagged_key_value_series import JaggedKeyValueSeries
+
+COLUMN_TYPES = (int, np.int, np.int64, str)
+
+
+class JaggedKeyValueFrame(object):
+
+    def __init__(self, arrs=None, index=None):
+        """
+
+        :param arrs dict of JaggedKeyValueArrays:
+        :param index shared by all arrays
+        :param columns: labels for each array
+        """
+
+        self.arrs =arrs
+        self.index = index
+
+        if index is None:
+            first_arr_name = sorted(arrs.keys())[0]
+            first_arr = arrs[first_arr_name]
+            self.index = np.arange(len(first_arr))
+        elif isinstance(index, (list, tuple)):
+            self.index = np.array(index)
+
+        self._verify()
+
+    def _verify(self):
+        for k, arr in self.arrs.items():
+            assert isinstance(arr, JaggedKeyValueArray)
+            assert len(arr) == len(self.index), '%s!=%s'%(len(arr), len(self.index))
+        assert is_sorted(self.index), 'index should be sorted'
+    #
+    # def __bool__(self):
+    #     return bool(self.arr.keys.shape[0])
+    #
+    # def __len__(self):
+    #     return len(self.index)
+
+    # def __eq__(self, other):
+    #     if not isinstance(other, JaggedKeyValueFrame):
+    #         return False
+    #     if len(self) != len(other):
+    #         return False
+    #     if not np.array_equal(self.index, other.index):
+    #         return False
+    #     if self.arr != other.arr:
+    #         return False
+    #
+    #     return True
+
+    def __getitem__(self, i):
+
+        if isinstance(i, COLUMN_TYPES):
+            arr = self.arrs[i]
+            return JaggedKeyValueSeries(arr=arr, index=self.index)
+        elif isinstance(i, list):
+            arrs = dict((k, self.arrs[k]) for k in i)
+            return JaggedKeyValueFrame(arrs, index=self.index)
+    #
+    # def _to_string(self, index, arr):
+    #     irows = zip(index, arr)
+    #     rows = ['\t%s, %s, %s' % (idx, k, v) for (idx, (k, v)) in irows]
+    #     return '\n'.join(rows)
+
+    def cumsum(self):
+        new_array = self.arr.cumsum()
+        return JaggedKeyValueSeries(new_array, self.index)
+
+    def get_ohlcv_frame(self, freq):
+        dfs = OrderedDict((k, self[k].get_ohlcv_frame(freq)) for k in self.arrs)
+        return pd.concat(dfs, axis=1)
+
+    # def get_ohlcv_frame_by_date_index(self, freq):
+    #     resampled_index = get_resampled_datetime_index(self.date_index, freq)
+    #     ohlc = self.get_ohlc_by_date_index(freq)
+    #     v = self.get_v_by_date_index(freq)
+    #     df = pd.DataFrame(
+    #         ohlc,
+    #         index=resampled_index,
+    #         columns=['o', 'h', 'l', 'c']
+    #     )
+    #     df['v'] = v
+    #     return df
+
+    def get_v(self, interval):
+        """
+        Convert this array into Open/High/Low/Close bars
+        :param freq:
+        :return:
+        """
+        resampled_bounds = self.get_resample_index_bounds(interval)
+        return self._get_v(resampled_bounds)
+
+    def _get_v(self, resampled_bounds):
+
+        result = np.zeros(len(resampled_bounds), dtype=self.arr.values.dtype)
+
+        for i in range(0, len(resampled_bounds)):
+            open0 = resampled_bounds[i, 0]
+            close1 = resampled_bounds[i, 3]
+            result[i] = self.arr.values[open0:close1].sum()
+
+        return result
+
+    def get_ohlc(self, interval):
+        """
+        Convert this array into Open/High/Low/Close bars
+        :param freq:
+        :return:
+        """
+        resampled_bounds = self.get_resample_index_bounds(interval)
+        return self._get_ohlc(resampled_bounds)
+
+    def _get_ohlc(self, resampled_bounds):
+
+        # Needs to be a float so that we can have nans
+        result = np.empty((len(resampled_bounds), 4), dtype=np.float)
+        result[:] = np.nan
+
+        for i in range(0, len(resampled_bounds)):
+
+            open0 = resampled_bounds[i, 0]
+            open1 = resampled_bounds[i, 1]
+            close0 = resampled_bounds[i, 2]
+            close1 = resampled_bounds[i, 3]
+
+            # High and Low
+            if close1 > open0:
+                all_values = self.arr.keys[open0:close1]
+                result[i, 1] = all_values.max()
+                result[i, 2] = all_values.min()
+            else:
+                result[i, 1] = np.nan
+                result[i, 2] = np.nan
+
+            # Open
+            opening_values = self.arr.keys[open0:open1]
+            result[i, 0] = modified_median(opening_values)
+
+            # Close
+            closing_values = self.arr.keys[close0:close1]
+            result[i, 3] = modified_median(closing_values)
+
+        return result
+
+    def get_active_keys(self):
+        """
+        Return the keys that are used by a jagged array
+
+        This is required, as if an array is a view of another, then some
+        of the keys could be unused
+        """
+        all_keys = self.keys[self.bounds[0]:self.bounds[-1]]
+        return np.unique(all_keys)
+
+    def get_resample_index_bounds(self, interval):
+
+        floored = self.index // interval
+        return self._get_resample_bounds(floored)
+
+    def _get_resample_bounds(self, floored_index):
+
+        i1 = np.where(np.diff(floored_index))[0] + 1
+        i0 = np.array([0])
+
+        open_bound_start_indices = np.r_[i0, i1]
+        open_bound_end_indices = open_bound_start_indices + 1
+
+        close_bound_start_indices = open_bound_start_indices[1:] - 1
+        close_bound_end_indices = close_bound_start_indices + 1
+
+        result = np.empty((len(open_bound_start_indices), 4), dtype=np.int)
+        result[:, 0] = self.arr.bounds[open_bound_start_indices]
+        result[:, 1] = self.arr.bounds[open_bound_end_indices]
+        result[:-1, 2] = self.arr.bounds[close_bound_start_indices]
+        result[-1, 2] = self.arr.bounds[-2]
+        result[:-1, 3] = self.arr.bounds[close_bound_end_indices]
+        result[-1, 3] = self.arr.bounds[-1]
+
+        return result
+
+    def ravel(self):
+        """
+        Return a representation consisting of 3 arrays
+         - index
+         - keys
+         - values
+        :return:
+        """
+
+        bounds = self.arr.bounds
+        length = bounds[-1] - bounds[0]
+        index = np.ndarray(length, dtype=self.index.dtype)
+        lower_bound = bounds[0]
+        for i in range(len(self)):
+            b0 = bounds[i]
+            b1 = bounds[i + 1]
+            i0 = b0 - lower_bound
+            i1 = b1 - lower_bound
+            index[i0:i1] = self.index[i]
+
+        values = self.arr.values[bounds[0]:bounds[-1]]
+        keys = self.arr.keys[bounds[0]:bounds[-1]]
+
+        return index, keys, values
+
+    def resample(self, freq):
+        """
+        Resample this, bucketting into data points floored to integer multiples
+        of freq
+        :param freq:
+        :return:
+        """
+        floored = self.index // freq
+        indices = get_change_indices(floored)
+
+        cs = self.arr.cumsum()
+
+        data, unique_keys = cs.to_dense()
+
+        old_row = np.zeros_like(unique_keys, dtype=np.int)
+
+        row_diffs = []
+        for i in indices[1:] - 1:
+            row = data[i]
+            row_diff = row - old_row
+            row_diffs.append(row_diff)
+            old_row = row
+
+        last_row = data[-1]
+        last_row_diff = last_row - old_row
+        row_diffs.append(last_row_diff)
+
+        diff_data = np.r_[row_diffs]
+
+        arr = JaggedKeyValueArray.from_dense(diff_data, unique_keys)
+        return JaggedKeyValueSeries(arr, index=self.index[indices])
+
+    @staticmethod
+    def load(filename):
+
+        with np.load(filename) as data:
+            return JaggedKeyValueSeries(
+                keys = data['keys'],
+                values = data['values'],
+                bounds = data['bounds'],
+                index = data['index'],
+            )
+
+    def save(self, filename):
+        """
+        Save to a file
+        :return:
+        """
+
+        np.savez_compressed(
+            filename,
+            keys = self.arr.keys,
+            values = self.arr.values,
+            bounds = self.arr.bounds,
+            index = self.index
+        )
+
+
+def modified_median(x):
+    """
+    Return the median if x has an odd number of values
+    if x has an odd number of values, return the lower of median values
+    :param x list: sorted list of objects
+    :return: median
+    """
+    if len(x):
+        return x[(-1 + x.shape[0]) // 2]
+    else:
+        return np.nan
+
+
+# def get_resampled_datetime_index(date_range, freq):
+#     """
+#     Return a date_range, resampled
+#     :param date_range:
+#     :param freq:
+#     :return:
+#     """
+#     floored = date_range.floor(freq)
+#     changed_indices = get_change_indices(floored)
+#     return date_range[changed_indices]
+
+
+def get_resampled_index(index, interval):
+    """
+    Resample the index, returning an array
+    :param index: np.ndarray
+    :param interval: int
+    :return: np.ndarray
+    """
+    resampled_indices = get_resample_indices(index, interval)
+    return index[resampled_indices]
+
+
+def get_resample_indices(index, interval):
+    """
+    Return the integer indices representing the 1st index of each resampled
+    bin. i.e. the index that would represent the open of an ohlc bar.
+    :param index:
+    :param freq:
+    :return:
+    """
+    # TODO: Might be inefficient, as get_resampled_indices could be called
+    # in the parent
+    floored = floor_to_nearest_int(index, interval)
+    return get_change_indices(floored)
+
+
+@nb.jit(nopython=True)
+def floor_to_nearest_int(x, multiplier):
+    """
+    Floor each value of x to the nearest multiple of multiplier
+    :param x: array
+    :param multiplier:
+    :return:
+    """
+    return multiplier * (x // multiplier)
+
+
+def get_change_indices(x):
+    i1 = np.where(np.diff(x))[0] + 1
+    i0 = np.array([0])
+    return np.r_[i0, i1]
+
+
+@nb.jit(nopython=True)
+def is_sorted(a):
+    for i in range(a.size - 1):
+        if a[i + 1] < a[i]:
+            return False
+    return True
--- jagged_array/tests/test_jagged_key_value_frame.py
+++ jagged_array/tests/test_jagged_key_value_frame.py
@@ -1,30 +1,72 @@
 import numpy as np
 import pandas as pd
+import pandas.util.testing as tm
 import tempfile
 import unittest
 
+from jagged_array.jagged_key_value_array import JaggedKeyValueArray
 from jagged_array.jagged_key_value_series import JaggedKeyValueSeries
-from jagged_array.jagged_key_value_array import JaggedKeyValueArray
-from jagged_array.jagged_key_value_array import JaggedKeyValueArray
 from jagged_array.jagged_key_value_frame import JaggedKeyValueFrame
 
 
 class JaggedKeyValueFrameTests(unittest.TestCase):
-    def test_xxx(self):
+
+    def setUp(self):
         data = {
-            '123': JaggedKeyValueArray(
-                keys=[200.0, 300.0, 400.0],
-                values=[1.0, 1.0, 1.0],
+            123: JaggedKeyValueArray(
+                keys=[2, 3, 4],
+                values=[1, 2, 3],
                 bounds=[0, 1, 2, 3],
             )
             ,
-            '567': JaggedKeyValueArray(
-                keys=[400.0, 500.0, 600.0],
-                values=[1.0, 1.0, 1.0],
+            567: JaggedKeyValueArray(
+                keys=[4, 5, 6],
+                values=[3, 2, 1],
                 bounds=[0, 1, 2, 3],
             )
         }
 
         index = [10, 11, 12]
 
-        jf = JaggedKeyValueFrame(data, index)
+        self.jf = JaggedKeyValueFrame(data, index)
+
+    def test___getitem__single(self):
+
+        result = self.jf[123]
+        self.assertIsInstance(result, JaggedKeyValueSeries)
+
+    def test_get_item__list(self):
+
+        result = self.jf[[123, 567]]
+        self.assertIsInstance(result, JaggedKeyValueFrame)
+
+    def test_get_ohlcv_frame(self):
+
+        expected = pd.DataFrame(
+            [
+            [2, 2, 2, 2, 1, 4, 4, 4, 4, 3],
+            [3, 3, 3, 3, 2, 5, 5, 5, 5, 2],
+            [4, 4, 4, 4, 3, 6, 6, 6, 6, 1],
+            ],
+            index = [10, 11, 12],
+        )
+        columns = pd.MultiIndex.from_tuples(
+            (
+            (123, 'o'),
+            (123, 'h'),
+            (123, 'l'),
+            (123, 'c'),
+            (123, 'v'),
+            (567, 'o'),
+            (567, 'h'),
+            (567, 'l'),
+            (567, 'c'),
+            (567, 'v'),
+            )
+        )
+        expected.columns = columns
+
+        print(expected)
+        df = self.jf.get_ohlcv_frame(1)
+        print(df)
+        tm.assert_frame_equal(expected, df, check_dtype=False)
